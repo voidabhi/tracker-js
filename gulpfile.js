d0270edf2385a0cb9819b08cc898197ff979267f
Abhijeet Mohan <void.aby@gmail.com>
2016-01-23 22:27:53 +0530

    add jshint task

--- gulpfile.js
+++ gulpfile.js
@@ -1,1 +1,11 @@
 
+
+var gulp = require('gulp'),
+    jshint = require('gulp-jshint');
+    
+    
+gulp.task('lint', function () {
+    gulp.src('**/*.js')
+        .pipe(jshint())
+        .pipe(jshint.reporter('default'));
+});
